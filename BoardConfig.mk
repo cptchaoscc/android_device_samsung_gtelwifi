cce7433e54e89fd15322de2eb232b4120749bf25
Leo Marchant <leo@marchant.se>
2021-05-21 22:40:38 +0200

    update to pie

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,8 +23,10 @@ TARGET_SCREEN_WIDTH := 1280
 
 # SPRD hardware
 BOARD_USES_SPRD_HARDWARE := true
-COMMON_GLOBAL_CFLAGS += -DSPRD_HARDWARE
 SOC_SCX30G_V2 := true
+
+# CMHW
+BOARD_HARDWARE_CLASS := device/samsung/sprd-common/cmhw/
 
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := SC7730SE
@@ -37,6 +39,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
+BOARD_VENDOR := samsung
 
 BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8,androidboot.hardware=sc8830
 BOARD_KERNEL_BASE := 0x00000000
@@ -47,6 +50,9 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_IMAGE_NAME := zImage
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androidkernel-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
 TARGET_CUSTOM_DTBTOOL := dtbToolSPRD
 
 TARGET_KERNEL_ARCH := arm
@@ -71,9 +77,11 @@ BOARD_HAVE_FM_BCM := true
 BOARD_HAVE_FMRADIO_BCM := true
 
 # Bluetooth
+BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+USE_BLUETOOTH_BCM4343 := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := $(DEVICE_PATH)/bluetooth/libbt_vndcfg.txt
+BOARD_CUSTOM_BT_CONFIG := $(DEVICE_PATH)/bluetooth/libbt_vndcfg.txt
 
 # Camera
 #USE_CAMERA_STUB := true
@@ -83,10 +91,10 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_NO_CHARGER_LED := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
-BACKLIGHT_PATH := /sys/class/backlight/sprd_backlight/brightness
+BACKLIGHT_PATH := /sys/class/backlight/panel/brightness
 
 # Codecs
-COMMON_GLOBAL_CFLAGS += -DBOARD_CANT_REALLOCATE_OMX_BUFFERS
+BOARD_CANT_REALLOCATE_OMX_BUFFERS := true
 
 # Graphics
 USE_OPENGL_RENDERER := true
@@ -95,10 +103,11 @@ TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 BOARD_EGL_NEEDS_HANDLE_VALUE := true
 HWUI_COMPILE_FOR_PERF := true
 TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
-COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH
+TARGET_FORCE_SCREENSHOT_CPU_PATH := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 # Enable WEBGL in WebKit
 ENABLE_WEBGL := true
+
 
 # HWComposer
 USE_SPRD_HWCOMPOSER := true
@@ -110,6 +119,8 @@ TARGET_POWERHAL_VARIANT := scx35
 
 # RIL
 BOARD_PROVIDES_RILD := true
+BOARD_RIL_CLASS := ../../../device/samsung/sprd-common/ril
+TARGET_DISABLE_ASHMEM_TRACKING := true
 
 # Wifi
 BOARD_WLAN_DEVICE := bcmdhd
@@ -130,19 +141,47 @@ BOARD_HAVE_SAMSUNG_WIFI := true
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
-# Use dmalloc() for such low memory devices like us
-#MALLOC_IMPL := dlmalloc
+# Bionic
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+
+# Build system
+WITHOUT_CHECK_API := true
+
+# Lights
+TARGET_HAS_BACKLIT_KEYS := false
+
+# Target shims
+TARGET_LD_SHIM_LIBS := \
+	/system/vendor/bin/gpsd|libgps_shim.so
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_sec
+
+# Board specific features
+#TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+
+# healthd
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.sc8830
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
+
+# Use MALLOC_SVELTE for such low memory devices like us
 MALLOC_SVELTE := true
 BOARD_USES_LEGACY_MMAP := true
 
-# Enable dex-preoptimization to speed up the first boot sequence
+# Enable dex-preoptimization to speed up first boot sequence
 WITH_DEXPREOPT := true
-WITH_DEXPREOPT_BOOT_IMG_ONLY := true
-WITH_DEXPREOPT_PIC := true
-WITH_DEXPREOPT_COMP := false
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+PRODUCT_DEX_PREOPT_BOOT_FLAGS += --compiler-filter=speed
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed
+WITH_DEX_PREOPT_GENERATE_PROFILE := false
+
+# System properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/boot/fstab.sc8830
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.sc8830
 BOARD_HAS_DOWNLOAD_MODE := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_HAS_LARGE_FILESYSTEM := true
--- arrow_gtelwifi.mk
+++ arrow_gtelwifi.mk
@@ -29,13 +29,6 @@ PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_LOCALES += hdpi
 
-# Rootdir
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,${LOCAL_PATH}/rootdir/boot,root)
-
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,${LOCAL_PATH}/rootdir/recovery,recovery/root)
-
 # Audio
 PRODUCT_PACKAGES += \
     audio.primary.sc8830 \
@@ -46,79 +39,182 @@ PRODUCT_PACKAGES += \
     libtinyalsa \
     libaudio-resampler
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
-    $(LOCAL_PATH)/configs/audio/audio_hw.xml:system/etc/audio_hw.xml \
-    $(LOCAL_PATH)/configs/audio/audio_para:system/etc/audio_para \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/codec_pga.xml:system/etc/codec_pga.xml \
-    $(LOCAL_PATH)/configs/audio/tiny_hw.xml:system/etc/tiny_hw.xml
+# Audio
+PRODUCT_PACKAGES += \
+	audio.a2dp.default \
+	audio.usb.default \
+	audio.r_submix.default \
+	libtinyalsa
+    audio_hw.xml \
+	audio_para \
+	audio_effects_vendor.conf \
+	audio_policy.conf \
+	codec_pga.xml \
+	tiny_hw.xml \
+	audio.primary.sc8830 \
+	libaudio-resampler
+
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    libbt-vendor
+    libbluetooth_jni \
+	bluetooth.default \
+	libbt-vendor
 
 # Camera
-ifneq ($(USE_CAMERA_STUB),true)
 PRODUCT_PACKAGES += \
-    camera.sc8830
+	Snap \
+	camera.sc8830
 
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=1
-endif
 
 # Charger
 PRODUCT_PACKAGES += \
-    charger \
-    charger_res_images
+	charger \
+	charger_res_images
 
 # Codecs
 PRODUCT_PACKAGES += \
-    libcolorformat_switcher \
-    libstagefrighthw \
-    libstagefright_sprd_mpeg4dec \
-    libstagefright_sprd_mpeg4enc \
-    libstagefright_sprd_h264dec \
-    libstagefright_sprd_h264enc \
-    libstagefright_sprd_vpxdec
+	libcolorformat_switcher \
+	libstagefrighthw \
+	libstagefright_sprd_mpeg4dec \
+	libstagefright_sprd_mpeg4enc \
+	libstagefright_sprd_h264dec \
+	libstagefright_sprd_h264enc \
+	libstagefright_sprd_vpxdec \
+	libstagefright_sprd_aacdec \
+	libstagefright_sprd_mp3dec
 
-# Compat
+# seccomp
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
+# Common libs
 PRODUCT_PACKAGES += \
-    libstlport
+	libstlport \
+	librilutils \
+	libril_shim \
+	libgps_shim \
+	libphoneserver_shim
+
+# RIL
+PRODUCT_PACKAGES += \
+	libatchannel \
+	libsecril-client \
+	libsecril-shim \
+	libril \
+	rild \
+	modemd \
+	modem_control
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.radio.modemtype=w \
+	rild.libpath=/system/vendor/lib/libsecril-shim.so
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/system/etc/init/rild.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/rild.rc
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-    setup_fs
+	f2fstat \
+	fibmap.f2fs \
+	fsck.f2fs \
+	mkfs.f2fs \
+	setup_fs \
+
+# Misc packages
+PRODUCT_PACKAGES += \
+	com.android.future.usb.accessory \
+    
+# Samsung Service Mode
+PRODUCT_PACKAGES += \
+	SamsungServiceMode
 
 # Graphics
 PRODUCT_PACKAGES += \
+    libHWCUtils \
     gralloc.sc8830 \
     hwcomposer.sc8830 \
     sprd_gsp.sc8830 \
     libmemoryheapion \
     libion_sprd
 
+# GPS
+PRODUCT_PACKAGES += \
+	libgpspc \
+	libefuse \
+	gps.conf \
+	gps.xml
+
+
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/configs/keylayout/headset-keyboard.kl:system/usr/keylayout/headset-keyboard.kl \
-    $(LOCAL_PATH)/configs/keylayout/sci-keypad.kl:system/usr/keylayout/sci-keypad.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/headset-keyboard.kl:system/usr/keylayout/headset-keyboard.kl \
+    $(LOCAL_PATH)/keylayout/sci-keypad.kl:system/usr/keylayout/sci-keypad.kl
+
+# System init.rc files
+PRODUCT_PACKAGES += \
+	at_distributor.rc \
+	chown_service.rc \
+	data.rc \
+	dns.rc \
+	engpc.rc \
+	gpsd.rc \
+	kill_phone.rc \
+	macloader.rc \
+	modem_control.rc \
+	modemd.rc \
+	nvitemd.rc \
+	phoneserver.rc \
+	refnotify.rc \
+	set_mac.rc \
+	smd_symlink.rc \
+	swap.rc \
+	wpa_supplicant.rc
+
+# Rootdir files
+PRODUCT_PACKAGES += \
+	init.board.rc \
+	init.wifi.rc \
+	init.sc8830.rc \
+	init.sc8830.usb.rc \
+	ueventd.sc8830.rc
+
+# Packages
+PRODUCT_PACKAGES += \
+	Jelly
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.sc8830
+	lights.sc8830
+
+# Rootdir
+PRODUCT_PACKAGES += \
+	fstab.sc8830
+
+# Interactive governor configs
+PRODUCT_PACKAGES += \
+	interactive_gov.rc
 
 # Media config
+PRODUCT_PACKAGES += \
+	media_codecs.xml
+    media_profiles_V1_0.xml
+
 MEDIA_XML_CONFIGS := \
-    $(LOCAL_PATH)/configs/media/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media/media_profiles.xml
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml
 
 PRODUCT_COPY_FILES += \
-    $(foreach f,$(MEDIA_XML_CONFIGS),$(f):system/etc/$(notdir $(f)))
+	$(foreach f,$(MEDIA_XML_CONFIGS),$(f):$(TARGET_COPY_OUT_VENDOR)/etc/$(notdir $(f)))
 
-# Misc packages
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/system/etc/init/android.hardware.media.omx@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.media.omx@1.0-service.rc \
+	$(LOCAL_PATH)/system/etc/init/mediaserver.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/mediaserver.rc
+
 
 # Permissions
 PERMISSION_XML_FILES := \
@@ -129,6 +225,7 @@ PERMISSION_XML_FILES := \
     frameworks/native/data/etc/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.xml \
     frameworks/native/data/etc/android.software.sip.xml \
@@ -139,10 +236,13 @@ PERMISSION_XML_FILES := \
     frameworks/native/data/etc/android.hardware.camera.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.software.midi.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml
 
 PRODUCT_COPY_FILES += \
     $(foreach f,$(PERMISSION_XML_FILES),$(f):system/etc/permissions/$(notdir $(f)))
+	
 
 # PowerHAL
 PRODUCT_PACKAGES += \
@@ -155,39 +255,79 @@ PRODUCT_PACKAGES += \
     hostapd \
     macloader
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
-    $(LOCAL_PATH)/configs/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
-    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+# Languages
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.product.locale.language=en \
+	ro.product.locale.region=GB
 
-# Set default USB interface
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
+# Wifi
+PRODUCT_PACKAGES += \
+	macloader \
+	libandroid_net \
+	libwpa_client \
+	wificond \
+	wifilogd \
+	wpa_supplicant.conf \
+	wpa_supplicant_overlay.conf \
+	p2p_supplicant_overlay.conf
+
+# Disable mobile data on first boot
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.com.android.mobiledata=false
+
+# Disable treble OMX
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.media.treble_omx=false
+
+# HIDL (HAL Interface Definition Language)
+include $(LOCAL_PATH)/hidl.mk
+
+# Performance
+PRODUCT_PROPERTY_OVERRIDES += \
+	sys.use_fifo_ui=1
+
 
 # ART device props
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.fw.dex2oat_thread_count=4 \
-    dalvik.vm.dex2oat-flags=--no-watch-dog
+	dalvik.vm.dex2oat-flags=--no-watch-dog \
+	dalvik.vm.image-dex2oat-filter=quicken \
+	dalvik.vm.dex2oat-filter=quicken \
+	pm.dexopt.first-boot=quicken \
+	pm.dexopt.boot=verify \
+	pm.dexopt.install=verify \
+	pm.dexopt.bg-dexopt=quicken \
+	pm.dexopt.ab-ota=quicken \
+	pm.dexopt.inactive=verify \
+	pm.dexopt.shared=quicken
+
 
 # enable Google-specific location features,
 # like NetworkLocationProvider and LocationCollector
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.locationfeatures=1 \
-    ro.com.google.networklocation=1
+	ro.com.google.locationfeatures=1 \
+	ro.com.google.networklocation=1
+
 
 # Dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
+
 # For userdebug builds
-ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.secure=0 \
-    ro.adb.secure=0 \
-    ro.debuggable=1 \
-    persist.sys.root_access=1 \
-    persist.service.adb.enable=1 \
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.secure=0 \
+	ro.adb.secure=0 \
+	ro.debuggable=1 \
+	persist.sys.root_access=1 \
+	persist.service.adb.enable=1 \
     ro.boot.selinux=permissive
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp,adb
+
+# Android Go
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.low_ram=true
 
 PRODUCT_NAME := arrow_gtelwifi
 PRODUCT_DEVICE := gtelwifi
